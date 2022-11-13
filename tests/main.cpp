0f1563567eb70d15fc90da9cb31aa21fff65548a
Elviss Strazdins <elviss@elviss.lv>
2022-11-13 12:30:24 +0200

    Use require

--- tests/main.cpp
+++ tests/main.cpp
@@ -22,9 +22,9 @@ TEST_CASE("AutoreleasePool")
     ns::AutoreleasePool autoreleasePool;
     mtl::Device device;
     ns::String name = device.name();
-    CHECK(name.retainCount() == 2);
+    REQUIRE(name.retainCount() == 2);
     autoreleasePool.drain();
-    CHECK(name.retainCount() == 1);
+    REQUIRE(name.retainCount() == 1);
 }
 
 TEST_CASE("Array")
@@ -33,7 +33,7 @@ TEST_CASE("Array")
     ns::Object obj2;
     const ns::Array<ns::Object> arr{obj1, obj2};
     CHECK(arr.retainCount() == 1);
-    CHECK(arr.count() == 2);
+    REQUIRE(arr.count() == 2);
 
     CHECK(obj1.retainCount() == 2);
     CHECK(obj2.retainCount() == 2);
@@ -53,7 +53,7 @@ TEST_CASE("Dictionary")
 
     const ns::Dictionary<ns::String, ns::Object> dict{obj1, key1, obj2, key2};
     CHECK(dict.retainCount() == 1);
-    CHECK(dict.count() == 2);
+    REQUIRE(dict.count() == 2);
     CHECK(obj1.retainCount() == 2);
     CHECK(obj2.retainCount() == 2);
 
@@ -81,7 +81,8 @@ TEST_CASE("Dictionary")
 TEST_CASE("String")
 {
     const ns::String str = "ab";
-    CHECK(str.length() == 2);
+    REQUIRE(str.retainCount() > 0);
+    REQUIRE(str.length() == 2);
     CHECK(str[0] == 'a');
     CHECK(str[1] == 'b');
     CHECK(str.charAtIndex(0) == 'a');
