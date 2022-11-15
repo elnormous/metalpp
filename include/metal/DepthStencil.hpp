14261bc358732995875cced9933059449f8e54f4
Elviss Strazdins <elviss@elviss.lv>
2022-11-15 16:42:15 +0200

    Add CompareFunction and StencilOperation enums

--- include/metal/DepthStencil.hpp
+++ include/metal/DepthStencil.hpp
@@ -1,6 +1,7 @@
 #ifndef METALPP_METAL_DEPTHSTENCIL_HPP
 #define METALPP_METAL_DEPTHSTENCIL_HPP
 
+#include <os/availability.h>
 #include "../objc/Object.hpp"
 #include "../objc/Selectors.hpp"
 #include "../foundation/String.hpp"
@@ -10,6 +11,30 @@
 namespace mtl
 {
     class Device;
+
+    enum class CompareFunction: NSUInteger
+    {
+        Never = 0,
+        Less = 1,
+        Equal = 2,
+        LessEqual = 3,
+        Greater = 4,
+        NotEqual = 5,
+        GreaterEqual = 6,
+        Always = 7,
+    } API_AVAILABLE(macos(10.11), ios(8.0));
+
+    enum class StencilOperation: NSUInteger
+    {
+        Keep = 0,
+        Zero = 1,
+        Replace = 2,
+        IncrementClamp = 3,
+        DecrementClamp = 4,
+        Invert = 5,
+        IncrementWrap = 6,
+        DecrementWrap = 7,
+    } API_AVAILABLE(macos(10.11), ios(8.0));
 
     class DepthStencilDescriptor final: public ns::Object
     {
