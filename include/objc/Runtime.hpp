a96be0f6d3a05645ae350bf95282cc200c82b021
Elviss Strazdins <elviss@elviss.lv>
2022-11-22 11:33:13 +0200

    Add constants for integer min and max

--- include/objc/Runtime.hpp
+++ include/objc/Runtime.hpp
@@ -7,6 +7,10 @@ namespace ns
 {
     using Integer = NSInteger;
     using UInteger = NSUInteger;
+
+    constexpr Integer IntegerMax = NSIntegerMax;
+    constexpr Integer IntegerMin = NSIntegerMin;
+    constexpr UInteger UIntegerMax = NSUIntegerMax;
 }
 
 #endif
