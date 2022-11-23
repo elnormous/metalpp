0b2976e7371d49b6d4c7d8cbb14f1969b57ed701
Elviss Strazdins <elviss@elviss.lv>
2022-11-23 11:59:06 +0200

    Add tests for layer filters

--- tests/quartzcore.cpp
+++ tests/quartzcore.cpp
@@ -10,6 +10,15 @@ TEST_CASE("Layer")
     REQUIRE(layer.getClass() == objc_getClass("CALayer"));
     layer.setFrame(cg::Rect{0, 0, 100, 100});
     CHECK(layer.frame() == cg::Rect{0, 0, 100, 100});
+    layer.setMinificationFilter(ca::filterLinear);
+    CHECK(layer.minificationFilter().isEqualToString(ca::filterLinear));
+    layer.setMinificationFilter(ca::filterNearest);
+    CHECK(layer.minificationFilter().isEqualToString(ca::filterNearest));
+
+    layer.setMagnificationFilter(ca::filterLinear);
+    CHECK(layer.magnificationFilter().isEqualToString(ca::filterLinear));
+    layer.setMagnificationFilter(ca::filterNearest);
+    CHECK(layer.magnificationFilter().isEqualToString(ca::filterNearest));
 }
 
 TEST_CASE("MetalLayer")
