bb5571e76e81bb8fa71ccc6cfccdd1ae540e8db5
Elviss Strazdins <elviss@elviss.lv>
2022-11-25 12:44:22 +0200

    Delete constructor of CommandQueue

--- include/metal/CommandQueue.hpp
+++ include/metal/CommandQueue.hpp
@@ -14,12 +14,7 @@ namespace mtl
     {
         static inline const auto cls = objc_lookUpClass("MTLCommandQueue");
     public:
-        using Object::Object;
-
-        CommandQueue() noexcept:
-            Object{objc::sendMessage<id>(objc::sendMessage<id>(cls, ns::sel::alloc), ns::sel::init)}
-        {
-        }
+        CommandQueue() = delete;
 
         [[nodiscard]] Device device() const noexcept;
         
