f96a873b2bba7c9f47a7170684abbe38edc399af
Elviss Strazdins <elviss@elviss.lv>
2022-11-27 12:28:32 +0200

    Use ns types

--- include/metal/Sampler.hpp
+++ include/metal/Sampler.hpp
@@ -9,27 +9,27 @@ namespace mtl
 {
     class Device;
 
-    enum class SamplerMinMagFilter: NSUInteger
+    enum class SamplerMinMagFilter: ns::UInteger
     {
         Nearest = 0,
         Linear = 1,
     } API_AVAILABLE(macos(10.11), ios(8.0));
 
-    enum class SamplerMipFilter: NSUInteger
+    enum class SamplerMipFilter: ns::UInteger
     {
         NotMipmapped = 0,
         Nearest = 1,
         Linear = 2,
     } API_AVAILABLE(macos(10.11), ios(8.0));
 
-    enum class SamplerBorderColor: NSUInteger
+    enum class SamplerBorderColor: ns::UInteger
     {
         TransparentBlack = 0,  // {0,0,0,0}
         OpaqueBlack = 1,       // {0,0,0,1}
         OpaqueWhite = 2,       // {1,1,1,1}
     } API_AVAILABLE(macos(10.12), ios(14.0));
 
-    enum class SamplerAddressMode: NSUInteger
+    enum class SamplerAddressMode: ns::UInteger
     {
         ClampToEdge = 0,
         MirrorClampToEdge API_AVAILABLE(macos(10.11), ios(14.0)) = 1,
