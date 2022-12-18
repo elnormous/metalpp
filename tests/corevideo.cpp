37a60ee33d8137628100a74bec84df86e179038b
Elviss Strazdins <elviss@elviss.lv>
2022-12-18 18:43:36 +0200

    Add test for DisplayLink callback

--- tests/corevideo.cpp
+++ tests/corevideo.cpp
@@ -1,11 +1,50 @@
+#include <condition_variable>
+#include <mutex>
 #include "doctest.h"
 #include "corevideo/DisplayLink.hpp"
+
+namespace
+{
+    struct Flag final
+    {
+        bool flag = false;
+        std::mutex mutex;
+        std::condition_variable condition;
+    };
+
+    CVReturn callback(CVDisplayLinkRef CV_NONNULL,
+                      const CVTimeStamp* CV_NONNULL,
+                      const CVTimeStamp* CV_NONNULL,
+                      CVOptionFlags,
+                      CVOptionFlags* CV_NONNULL,
+                      void* CV_NULLABLE displayLinkContext)
+    {
+        Flag& flag = *static_cast<Flag*>(displayLinkContext);
+
+        std::unique_lock lock{flag.mutex};
+        flag.flag = true;
+        lock.unlock();
+        flag.condition.notify_all();
+
+        return kCVReturnSuccess;
+    }
+}
 
 TEST_CASE("DisplayLink")
 {
     const auto mainDisplayId = cv::mainDisplayID();
-    CHECK(mainDisplayId);
+    REQUIRE(mainDisplayId);
 
     cv::DisplayLink displayLink{mainDisplayId};
-    CHECK(displayLink);
+    REQUIRE(displayLink);
+    CHECK(displayLink.retainCount());
+
+    Flag flag;
+    displayLink.setCallback(callback, &flag);
+    displayLink.start();
+
+    std::unique_lock lock{flag.mutex};
+    while (!flag.flag) flag.condition.wait(lock);
+
+    displayLink.stop();
 }
