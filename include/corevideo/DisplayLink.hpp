87934fcadae70f15ac78659be582fa2c231a5a35
Elviss Strazdins <elviss@elviss.lv>
2023-01-12 18:40:56 +0200

    Mark isRunning as noexcept

--- include/corevideo/DisplayLink.hpp
+++ include/corevideo/DisplayLink.hpp
@@ -101,7 +101,7 @@ namespace cv
                     throw std::system_error{result, errorCategory, "Failed to stop display link"};
         }
 
-        bool isRunning() const
+        bool isRunning() const noexcept
         {
             return static_cast<bool>(CVDisplayLinkIsRunning(ref));
         }
