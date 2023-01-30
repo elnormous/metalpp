189e9378ad3ca585d2c5ee2887f5396fb38ce35b
Elviss Strazdins <elviss@elviss.lv>
2023-01-30 16:26:13 +0200

    Add attributes to Drawable

--- include/metal/Drawable.hpp
+++ include/metal/Drawable.hpp
@@ -12,6 +12,8 @@ namespace mtl
     public:
         METALPP_PRIVATE_SEL(present, "present");
         METALPP_PRIVATE_SEL(presentAtTime_, "presentAtTime:");
+        METALPP_PRIVATE_SEL(presentedTime, "presentedTime");
+        METALPP_PRIVATE_SEL(drawableID, "drawableID");
 
         using Object::Object;
         using Object::operator=;
@@ -26,6 +28,16 @@ namespace mtl
         void present(const CFTimeInterval presentationTime) noexcept
         {
             sendMessage(METALPP_SEL(presentAtTime_), presentationTime);
+        }
+
+        [[nodiscard]] auto presentedTime() const noexcept API_AVAILABLE(macos(10.15.4), ios(10.3), macCatalyst(13.4))
+        {
+            return sendMessage<CFTimeInterval>(METALPP_SEL(presentedTime));
+        }
+
+        [[nodiscard]] auto drawableID() const noexcept API_AVAILABLE(macos(10.15.4), ios(10.3), macCatalyst(13.4))
+        {
+            return sendMessage<ns::UInteger>(METALPP_SEL(drawableID));
         }
     } API_AVAILABLE(macos(10.11), ios(8.0));
 }
