fe8977567b055dd64d7c96619a6da92dc9f4ba2f
Elviss Strazdins <elviss@elviss.lv>
2023-03-01 04:47:32 +0200

    Add conversion functions from CoreGraphics types and add tests

--- include/coregraphics/Geometry.hpp
+++ include/coregraphics/Geometry.hpp
@@ -9,6 +9,14 @@ namespace cg
     {
         Point() noexcept = default;
         Point(const Float px, const Float py) noexcept: x{px}, y{py} {}
+        Point(const CGPoint& p) noexcept: x{p.x}, y{p.y} {}
+
+        Point& operator=(const CGPoint& p) noexcept
+        {
+            x = p.x;
+            y = p.y;
+            return *this;
+        }
 
         [[nodiscard]] bool operator==(const Point& other) const noexcept
         {
@@ -16,6 +24,16 @@ namespace cg
         }
 
         [[nodiscard]] bool operator!=(const Point& other) const noexcept
+        {
+            return other.x != x || other.y != y;
+        }
+
+        [[nodiscard]] bool operator==(const CGPoint& other) const noexcept
+        {
+            return other.x == x && other.y == y;
+        }
+
+        [[nodiscard]] bool operator!=(const CGPoint& other) const noexcept
         {
             return other.x != x || other.y != y;
         }
@@ -28,6 +46,7 @@ namespace cg
     {
         Size() noexcept = default;
         Size(const Float w, const Float h) noexcept: width{w}, height{h} {}
+        Size(const CGSize& s) noexcept: width{s.width}, height{s.height} {}
 
         [[nodiscard]] bool operator==(const Size& other) const noexcept
         {
@@ -35,6 +54,16 @@ namespace cg
         }
 
         [[nodiscard]] bool operator!=(const Size& other) const noexcept
+        {
+            return other.width != width || other.height != height;
+        }
+
+        [[nodiscard]] bool operator==(const CGSize& other) const noexcept
+        {
+            return other.width == width && other.height == height;
+        }
+
+        [[nodiscard]] bool operator!=(const CGSize& other) const noexcept
         {
             return other.width != width || other.height != height;
         }
@@ -47,6 +76,7 @@ namespace cg
     {
         Vector() noexcept = default;
         Vector(const Float x, const Float y) noexcept: dx{x}, dy{y} {}
+        Vector(const CGVector& v) noexcept: dx{v.dx}, dy{v.dy} {}
 
         [[nodiscard]] bool operator==(const Vector& other) const noexcept
         {
@@ -54,6 +84,16 @@ namespace cg
         }
 
         [[nodiscard]] bool operator!=(const Vector& other) const noexcept
+        {
+            return other.dx != dx || other.dy != dy;
+        }
+
+        [[nodiscard]] bool operator==(const CGVector& other) const noexcept
+        {
+            return other.dx == dx && other.dy == dy;
+        }
+
+        [[nodiscard]] bool operator!=(const CGVector& other) const noexcept
         {
             return other.dx != dx || other.dy != dy;
         }
@@ -67,6 +107,7 @@ namespace cg
         Rect() noexcept = default;
         Rect(const Float x, const Float y, const Float w, const Float h) noexcept: origin{x, y}, size{w, h} {}
         Rect(const Point o, const Size s) noexcept: origin{o}, size{s} {}
+        Rect(const CGRect& r) noexcept: origin{r.origin.x, r.origin.y}, size{r.size.width, r.size.height} {}
 
         [[nodiscard]] bool operator==(const Rect& other) const noexcept
         {
--- tests/coregraphics.cpp
+++ tests/coregraphics.cpp
@@ -20,11 +20,61 @@ TEST_CASE("ColorSpace")
 
 TEST_CASE("Float")
 {
-    CGFloat f = 5.0;
-    cg::Float r = f;
+    CGFloat test = 5.0;
+    cg::Float r = test;
     CHECK(r == r);
     CHECK_FALSE(r != r);
-    CHECK(r == f);
-    CHECK_FALSE(r != f);
+    CHECK(r == test);
+    CHECK_FALSE(r != test);
     CHECK(r == 5.0);
+}
+
+TEST_CASE("Point")
+{
+    CGPoint test = CGPointMake(5.0, 6.0);
+    cg::Point r = test;
+    CHECK(r == r);
+    CHECK_FALSE(r != r);
+    CHECK(r == test);
+    CHECK_FALSE(r != test);
+    CHECK(r.x == 5.0);
+    CHECK(r.y == 6.0);
+}
+
+TEST_CASE("Size")
+{
+    CGSize test = CGSizeMake(5.0, 6.0);
+    cg::Size r = test;
+    CHECK(r == r);
+    CHECK_FALSE(r != r);
+    CHECK(r == test);
+    CHECK_FALSE(r != test);
+    CHECK(r.width == 5.0);
+    CHECK(r.height == 6.0);
+}
+
+TEST_CASE("Vector")
+{
+    CGVector test = CGVectorMake(5.0, 6.0);
+    cg::Vector r = test;
+    CHECK(r == r);
+    CHECK_FALSE(r != r);
+    CHECK(r == test);
+    CHECK_FALSE(r != test);
+    CHECK(r.dx == 5.0);
+    CHECK(r.dy == 6.0);
+}
+
+TEST_CASE("Rect")
+{
+    CGRect test = CGRectMake(5.0, 6.0, 7.0, 8.0);
+    cg::Rect r = test;
+    CHECK(r == r);
+    CHECK_FALSE(r != r);
+    CHECK(r == test);
+    CHECK_FALSE(r != test);
+    CHECK(r.origin.x == 5.0);
+    CHECK(r.origin.y == 6.0);
+    CHECK(r.size.width == 7.0);
+    CHECK(r.size.height == 8.0);
 }
